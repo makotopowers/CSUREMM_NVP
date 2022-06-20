38dd5f6bd9ce0f0cc206e5d439e000f63f276cc0
Ethan <ezt2102@columbia.edu>
2022-06-20 12:11:25 -0400

    create loss function

--- src/models/loss.py
+++ src/models/loss.py
@@ -1,23 +1,27 @@
 import numpy as np
+from icecream import ic
 
 
-def inventory_loss(y_true, y_pred, overage, underage):
-    """Loss function for computing the cost of having overage and underages in inventory.
+def cost(q, d, c_o, c_u):
+    """Compute the cost of producing order quantities q when the true demand was d for
+    each of n days. The cost on the ith day is:
+        cost = c_o * (q[i] - d[i] if =+ c_u * d[i]
 
     Parameters
     ----------
-    y_true : _type_
-        _description_
-    y_pred : _type_
-        _description_
-    overage : _type_
-        _description_
-    underage : _type_
-        _description_
+    d : array-like of shape (n, 1)
+        Demand for each day.
+    q : array-like of shape (n, 1)
+        Ordering quantity for each day.
+    c_o : float
+        Overage cost.
+    c_u : float
+        Underage cost.
 
     Returns
     -------
-    _type_
-        _description_
+    array-like of shape (n,)
+        The cost incurred on each day.
     """
-    return np.sum(y_true > y_pred) * underage + np.sum(y_true < y_pred) * overage
+
+    return c_u * np.squeeze((d-q)*[d > q]) + c_o * np.squeeze((q-d) * [q > d])
